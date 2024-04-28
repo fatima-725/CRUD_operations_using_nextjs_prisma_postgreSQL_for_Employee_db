6b527af641132cfc12febc36010272d123956972
fatima <fatima.tz8490@gmail.com>
2024-04-28 15:24:39 +0500

    Part 1 completed - Storing the data in form to database

--- actions/createEmployee.ts
+++ actions/createEmployee.ts
@@ -1,6 +1,29 @@
 "use server";
 
+import { db } from "@/lib/db";
+import { EmployeeSchema } from "@/schemas";
+import { z } from "zod"
+
 //server action to create employee
-export const createEmployee = async (values: any) => {
+export const createEmployee = async (values: z.infer<typeof EmployeeSchema>) => {
     console.log(values)
+
+    const validatedFields = EmployeeSchema.safeParse(values)
+
+    if( !validatedFields.success ){
+        return{
+            error: "Invalid Fields"
+        }
+    }
+
+    const employee = await db.employee.create({
+        data:{
+            firstname: validatedFields.data?.firstname,
+            lastname: validatedFields.data?.lastname,
+            email: validatedFields.data?.email,
+            phone: validatedFields.data?.phoneNumber,
+        }
+    })
+
+    console.log(employee)
 }
